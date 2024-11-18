9598f18d41a835d95ec9ae9deaae4d8e54873963
terrxrmachine <so3.workspace@gmail.com>
2024-11-18 10:43:28 +0700

    bksurbg

--- review-bot/src/components/ui/tabs.jsx
+++ review-bot/src/components/ui/tabs.jsx
@@ -41,3 +41,5 @@ const TabsContent = React.forwardRef(({ className, ...props }, ref) => (
 TabsContent.displayName = TabsPrimitive.Content.displayName;
 
 export { Tabs, TabsList, TabsTrigger, TabsContent };
+
+//fksnf
